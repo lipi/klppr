3ac261b6898dd00fde76aaafe3429fe6b9a35b29
lipi <lipi@ubuntu>
2015-04-04 10:34:01 +1300

    Moved layout definition to KV file

--- src/testapp.py
+++ src/testapp.py
@@ -8,101 +8,85 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 
 import jvc
 
-class TestApp(App):
+class CalibScreen(BoxLayout):
+
+    camera = ObjectProperty()
+    filename = StringProperty('current.jpg')
+    image = ObjectProperty()
+
+    pan = NumericProperty(0)
+    tilt = NumericProperty(0)
+    pan_speed = NumericProperty(1)
+    tilt_speed = NumericProperty(1)
 
     #
     # Camera control
     #
 
-    def up(self, instance):
+    def up(self):
         print 'up...',
         self.tilt += self.tilt_speed
         self.pantilt()
         print 'done'
 
-    def down(self, instance):
+    def down(self):
         print 'down...'
         self.tilt -= self.tilt_speed
         self.pantilt()
         print 'done'
 
-    def left(self, instance):
+    def left(self):
         print 'left...',
         self.pan -= self.pan_speed
         self.pantilt()
         print 'done'
 
-    def right(self, instance):
+    def right(self):
         print 'right...'
         self.pan += self.pan_speed
         self.pantilt()
         print 'done.'
 
     def pantilt(self):
-        self.jvc.pantilt(self.pan, self.tilt)
+        self.camera.pantilt(self.pan, self.tilt)
+        return
 
     def clock_callback(self, dt):
-        img = self.jvc.getjpg()
-        self.jvc.savejpg(img, self.filename)
+        print 'refresh..'
+        img = self.camera.getjpg()
+        self.camera.savejpg(img, self.filename)
         self.image.reload()
+
+
+
+class TestApp(App):
 
     #
     # Kivy
     #
 
     def on_start(self):
+        print 'start...'
         self.jvc = jvc.JVC('192.168.3.114')
         self.jvc.login()
+        self.calib_screen.camera = self.jvc
+        print 'started.'
 
-        self.pan = 0
-        self.tilt = 0
-        self.pan_speed = 1
-        self.tilt_speed = 1
     
     def build(self):
+        print 'building...'
+        self.calib_screen = CalibScreen()
+        Clock.schedule_interval(self.calib_screen.clock_callback, 0.5)
+        return self.calib_screen
 
-        control_layout = GridLayout(cols=3, rows=3)
-        left_button = Button(size_hint=(None, None),
-                             text='left', on_press=self.left)
-        right_button = Button(size_hint=(None, None),
-                              text='right', on_press=self.right)
-        up_button = Button(size_hint=(None, None),
-                               text='up', on_press=self.up)
-        down_button = Button(size_hint=(None, None),
-                               text='down', on_press=self.down)
-
-        # first row
-        control_layout.add_widget(Label()) # dummy
-        control_layout.add_widget(up_button)
-        control_layout.add_widget(Label()) # dummy
-        # second row
-        control_layout.add_widget(left_button)
-        control_layout.add_widget(Label()) # dummy
-        control_layout.add_widget(right_button)
-        # third row
-        control_layout.add_widget(Label()) # dummy
-        control_layout.add_widget(down_button)
-        control_layout.add_widget(Label()) # dummy
-
-        status_layout = BoxLayout(orientation='vertical')
-        self.filename = 'current.jpg'
-        image = Image(source=self.filename)
-        self.image = image
-        status_layout.add_widget(image)
-
-        top_layout = BoxLayout(orientation='vertical')
-        top_layout.add_widget(control_layout)
-        top_layout.add_widget(status_layout)
-
-        Clock.schedule_interval(self.clock_callback, 0.5)
-
-        return top_layout
 
     def on_stop(self):
         self.jvc.logout()
+        return
 
 if __name__ == '__main__':
     TestApp().run()
