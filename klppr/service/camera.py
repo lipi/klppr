92cf0333314d122baec5b54db4313352bf72a362
Andras Lipoth <andras.lipoth@gmail.com>
2015-08-18 22:34:59 +1200

    Refactored camera service and driver

---
+++ klppr/service/camera.py
@@ -0,0 +1,68 @@
+
+import pickle
+
+from kivy.lib import osc
+
+class CameraService(object):
+
+    def __init__(self, camera):
+
+        # initialize camera
+        self.camera = camera
+
+        # using OSC as Android doesn't support POSIX-style IPC
+        osc.init()
+        self._oscid = osc.listen(port=3000)
+        osc.bind(self._oscid, self._pantilt, '/pantilt')
+        osc.bind(self._oscid, self._zoom, '/zoom')
+        osc.bind(self._oscid, self._logout, '/logout')
+        osc.bind(self._oscid, self._stop_preview, '/stop_preview')
+        osc.bind(self._oscid, self._start_preview, '/start_preview')
+        osc.bind(self._oscid, self._get_ptz, '/get_ptz')
+
+        # notify UI of current PTZ state
+        osc.sendMsg('/ptz', [pickle.dumps(camera.ptz()),], port=3002)
+        # note: if service is already running when UI starts this
+        # won't happen -- remove?
+
+
+    def _pantilt(self, message, *args):
+        pan,tilt = pickle.loads(message[2]) 
+        self.camera.pantilt(pan,tilt)
+
+    def _zoom(self, message, *args):
+        level,speed = pickle.loads(message[2])
+        self.camera.zoom(level,speed)
+
+    def _logout(self, *args):
+        self.camera.close()
+        osc.dontListen()
+        exit()
+
+    def _stop_preview(self, *args):
+        self.camera.stop_preview()
+
+    def _start_preview(self, *args):
+        self.camera.start_preview()
+
+    def _get_ptz(self, *args):
+        osc.sendMsg('/ptz', [pickle.dumps(self.camera.ptz()),], port=3002)
+
+    def run(self):
+        while True:
+            # process incoming commands
+            try:
+                osc.readQueue(self._oscid)
+            except KeyError:
+                return
+
+            # send preview update
+            jpg = self.camera.getjpg()
+            if jpg is not None:
+                # NOTE: OSC is UDP based, therefore jpg must be 64k or less
+                # (otherwise it must be split to multiple transfers)
+                # Also note that the OS might drop UDP packets if its
+                # receive buffers are small (can be increased in OSCServer by 
+                # using setsockopt).
+                assert(len(jpg) < 64000)
+                osc.sendMsg('/jpg', [jpg,], port=3002, typehint='b')
--- klppr/service/main.py
+++ klppr/service/main.py
@@ -3,81 +3,18 @@
 # klppr service
 #
 
-from time import sleep
 import ConfigParser
-import pickle
 
-from kivy.lib import osc
-
-import jvc
-from camera  import AsyncCamera
-
-class CameraService(object):
-
-    def __init__(self, camera):
-        self.camera = camera
-
-        # using OSC as Android doesn't support POSIX-style IPC
-        osc.init()
-        self._oscid = osc.listen(port=3000)
-        osc.bind(self._oscid, self._pantilt, '/pantilt')
-        osc.bind(self._oscid, self._zoom, '/zoom')
-        osc.bind(self._oscid, self._logout, '/logout')
-        osc.bind(self._oscid, self._stop_preview, '/stop_preview')
-        osc.bind(self._oscid, self._start_preview, '/start_preview')
-        osc.bind(self._oscid, self._get_ptz, '/get_ptz')
-
-        # notify UI of current PTZ state
-        osc.sendMsg('/ptz', [pickle.dumps(camera.ptz()),], port=3002)
-        # note: if service is already running when UI starts this
-        # won't happen -- remove?
-
-    def _pantilt(self, message, *args):
-        pan,tilt = pickle.loads(message[2]) 
-        self.camera.pantilt(pan,tilt)
-
-    def _zoom(self, message, *args):
-        level,speed = pickle.loads(message[2])
-        self.camera.zoom(level,speed)
-
-    def _logout(self, *args):
-        self.camera.close()
-        osc.dontListen()
-        exit()
-
-    def _stop_preview(self, *args):
-        self.camera.stop_preview()
-
-    def _start_preview(self, *args):
-        self.camera.start_preview()
-
-    def _get_ptz(self, *args):
-        osc.sendMsg('/ptz', [pickle.dumps(self.camera.ptz()),], port=3002)
-
-    def run(self):
-        while True:
-            # process incoming commands
-            try:
-                osc.readQueue(self._oscid)
-            except KeyError:
-                return
-
-            # send preview update
-            jpg = camera.getjpg()
-            if jpg is not None:
-                # NOTE: OSC is UDP based, therefore jpg must be 64k or less
-                # (otherwise it must be split to multiple transfers)
-                # Also note that the OS might drop UDP packets if its
-                # receive buffers are small (can be increased in OSCServer by 
-                # using setsockopt).
-                assert(len(jpg) < 64000)
-                osc.sendMsg('/jpg', [jpg,], port=3002, typehint='b')
+from driver import jvc
+from driver.camera  import AsyncCamera
+from camera import CameraService
 
 if __name__ == '__main__':
     
     # initialze camera
     config = ConfigParser.RawConfigParser()
     config.read('camera.cfg')
+    print config.get('access', 'hostname')
 
     driver = jvc.JVC(host = config.get('access', 'hostname'),
                      user = config.get('access', 'user'),
@@ -87,5 +24,4 @@ if __name__ == '__main__':
     # start listening for commands from UI or tracker
     service = CameraService(camera)
     service.run()
-
 
